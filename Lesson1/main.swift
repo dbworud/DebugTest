573a3fff44355e00cfe57be5209d7a6d9c85e016
JaekyungYou <youjk96@gmail.com>
2020-12-16 14:54:44 +0900

    Lesson1: debugPrint

--- Lesson1/main.swift
+++ Lesson1/main.swift
@@ -7,5 +7,35 @@
 
 import Foundation
 
-print("Hello, World!")
+class Bar: NSObject {
+    override var description: String {
+        return "Bar Description"
+    }
+    
+    override var debugDescription: String {
+        return "Debug Bar Description"
+    }
+}
+
+class Foo: CustomStringConvertible, CustomDebugStringConvertible {
+    var description: String {
+        return "Foo Description"
+    }
+    
+    var debugDescription: String {
+        return "Debug Foo Description"
+    }
+    
+}
+
+let bar = Bar()
+let foo = Foo()
+
+print(bar) // description 출력
+debugPrint(bar) // debugDescription 출력
+
+print(foo)
+debugPrint(foo) // breakpoint를 걸어준 부분부터는 실행이 안 됨
+
+
 
---
+++ Lesson2/main.swift
@@ -0,0 +1,8 @@
+//
+//  main.swift
+//  Lesson1
+//
+//  Created by jaekyung you on 2020/12/16.
+//
+
+import Foundation
