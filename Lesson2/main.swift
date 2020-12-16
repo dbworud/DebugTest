bbc82fb7be130b7a452dbb72d420a31ac8e5833f
JaekyungYou <youjk96@gmail.com>
2020-12-16 16:03:18 +0900

    Lesson2: Breakpoint

--- Lesson2/main.swift
+++ Lesson2/main.swift
@@ -7,5 +7,38 @@
 
 import Foundation
 
-print("Hello, World!")
+class Person: CustomStringConvertible {
+    
+    var name: String
+    var age: Int
+    
+    init(name: String, age:Int) {
+        self.name = name
+        self.age = age
+    }
+    
+    var description: String {
+        return "\(name): \(age)"
+    }
+}
 
+let person1 = Person(name: "재경", age: 25)
+let person2 = Person(name: "한나", age: 30)
+let person3 = Person(name: "상민", age: 24)
+
+// breakpoint가 실행되는 조건(condition)을 설정할 수 있음
+let people = [person1, person2, person3]
+print(people)
+
+
+// conditional breakpoint
+for p in people {
+    print(p)
+}
+
+// move instruction pointer would be dangerous but sometimes needed
+var i = 0
+while i < 4 {
+    i += 1
+    print(i)
+}
