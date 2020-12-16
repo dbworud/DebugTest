a6839e320765f6097eb0eaea374619fc9595df59
JaekyungYou <youjk96@gmail.com>
2020-12-16 17:08:31 +0900

    Lesson3: Symbolic Breakpoint

--- Lesson3/ViewController.swift
+++ Lesson3/ViewController.swift
@@ -9,11 +9,29 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var countBtn: UIButton!
+    
+    @IBOutlet weak var countLabel: UITextField!
+    
+    var clickCounter = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        //view.addSubview(countBtn)
+        countBtn.addTarget(self, action: #selector(btnPressed(_:)), for: .touchUpInside)
     }
 
+    @objc fileprivate func btnPressed(_ sender: UIButton) {
+        countLabel.text = labelText()
+    }
+    
+    
+    func labelText() -> String {
+        clickCounter += 1
+        return "Number of clicks: \(clickCounter)"
+    }
 
 }
 
